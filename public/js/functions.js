7b76be6fbe45bd67c6f6717cea85c6606c745f41
Joshua Williams <joshuamw322@gmail.com>
2017-02-15 15:49:42 -0600

    loops and functions

---
+++ public/js/functions.js
@@ -0,0 +1,36 @@
+"use strict";
+
+// TODO: Joshua Williams
+
+console.log(hello);
+
+var myNameIs = "Joshua Williams"; 
+// TODO:			
+function sayHello(myNameIs) { 
+	console.log("Hello from " + myNameIs);
+}
+ sayHello(myNameIs);
+
+
+// example of function VVVV
+
+// function identify(input) {
+// console.log(input);
+// 			}
+
+
+// Create a function called 'sayHello' that takes a parameter 'name'.
+// When called, the function should log a message that says hello from the passed in name.
+
+// TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+
+// Don't modify the following line
+// It generates a random number between 1 and 100 and stores it in random
+var random = Math.floor((Math.random()*100)+1);
+
+// TODO:
+// Create a function called 'isOdd' that takes a number as a parameter.
+// The function should use the ternary operator to log a message.
+// The log should tell the number passed in and whether it is odd or not.
+
+// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
