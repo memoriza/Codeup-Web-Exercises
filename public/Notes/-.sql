76a60c89cd1edf3f63e3060ba6bd2a690ced5938
Joshua Williams <joshuamw322@gmail.com>
2017-02-27 16:04:55 -0600

    AJAX requests intro

---
+++ public/Notes/-.sql
@@ -0,0 +1,9 @@
+Google jquery UI CDN 
+
+or use this one
+
+
+<script
+        src="http://code.jquery.com/ui/1.12.1/jquery-ui.js"
+        integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+        crossorigin="anonymous"></script>
---
+++ public/Notes/________________________BA_HINT_HINT.sql
@@ -0,0 +1,7 @@
+Hints for the konami code
+
+1. Create a list of all the keycodes you are looking for for the konami code.
+2. Create an event listener to catch every key pressed, maybe creating a counter is a good idea, increase the value +1 everytime you type something.
+3. Check if the key that was pressed it's the next one in the list.
+4. And if the length of the list is the same as a counter that means you got it!
+5. Maybe it's a good idea to reset everything either if i got it or not.
